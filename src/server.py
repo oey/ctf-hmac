ea9c80975722fb75cb0876183f382918dc4506ec
Øyvind Hansen <oyvind.hansen@gmail.com>
2021-10-14 19:40:10 +0200

    Initial commit

---
+++ src/server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import socket, re
+import hmac, hashlib
+
+HOST = '' 
+PORT = 31337        # Port to listen on (non-privileged ports are > 1023)
+
+errorMessage = "Expected <message>;<auth> not ".encode()
+test1 = "The quick brown fox jumps over the lazy dog"
+testkey = "key".encode()
+publicKnownKey = "ourSharedSecret".encode()
+privateKey = "Port to listen on (non-privileged ports are > 1023)".encode()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+
+            elements = re.split(";", data.decode('utf-8').strip())
+            
+            if len(elements) == 2:
+                receivedMessage = elements[0]
+                receivedHmac = elements[1].lower()
+                messageHmac = hmac.new(testkey, receivedMessage.encode(), hashlib.sha256).hexdigest().lower()
+                taskHmac = hmac.new(privateKey, receivedMessage.encode(), hashlib.sha256).hexdigest().lower()
+                print(f"hmac('{testkey}','{receivedMessage.encode()}') = {messageHmac}")
+                if receivedHmac == messageHmac:                    
+                    conn.sendall("200 OK\n".encode())
+                    conn.sendall(b"Your knowledge about HMAC is verified.\n")
+                    conn.sendall(b"The following line can be used for proof of completed task.\n")
+                    conn.sendall(f"{receivedMessage};{taskHmac}\n".encode())
+                elif receivedHmac == taskHmac:
+                    conn.sendall("200 OK\n".encode())
+                    conn.sendall(b"The message below is authenticated and valid.\n")
+                    conn.sendall(f"{receivedMessage}\n".encode())
+                else:
+                    conn.sendall(f"401 Unauthorized {receivedMessage}\n".encode())
+            else:         
+                conn.sendall(errorMessage)
+                conn.sendall(data)
+                conn.sendall(b"\n")
